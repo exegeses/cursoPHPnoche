aceab9e52d5e12e8df7bbbc5f11ac1b9d230c834
marcos pinardi <exegeses@gmail.com>
2020-04-21 19:21:16 -0300

    clase 3

---
+++ inicial/clase3/condicionales.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Condicionales</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Tarea condicionales</h1>
+
+<?php
+
+?>
+
+</body>
+</html>
