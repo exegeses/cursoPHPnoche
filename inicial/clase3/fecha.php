679d271217126984baed7e1904b871b2a3b9cbc1
marcos pinardi <exegeses@gmail.com>
2020-04-21 20:14:32 -0300

    trabajo con fecha

---
+++ inicial/clase3/fecha.php
@@ -0,0 +1,19 @@
+<?php
+    date_default_timezone_set('America/Argentina/Buenos_Aires');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Manual oficial de PHP - Trabajo con fecha</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Manual oficial de PHP - Trabajo con fecha</h1>
+
+<?php
+    echo date('d');
+?>
+
+</body>
+</html>
---
+++ inicial/clase3/notas.md
@@ -0,0 +1,9 @@
+# Notas de clase
+
+>Si quiero hacer algo con PHP y no sé como hacerlo,  
+>¿dónde busco?  
+>en el manual oficial de PHP
+
+    http://www.php.net
+    
+    
