a8d527ecc8c0928d692fbc21961e5b552da1f34e
marcos pinardi <exegeses@gmail.com>
2020-04-21 19:26:19 -0300

    tarea condicionales resuelto

--- inicial/clase3/condicionales.php
+++ inicial/clase3/condicionales.php
@@ -9,8 +9,15 @@
     <h1>Tarea condicionales</h1>
 
 <?php
-
+    $numero = $_POST['numero'];
+    if( $numero < 100 ){
+        echo '<img src="imagenes/ok.png">';
+    }
+    else{
+        echo '<img src="imagenes/error.png">';
+    }
 ?>
+
 
 </body>
 </html>
