e693a25914c3e7b41a948ef4e3db516791e58b8c
marcos pinardi <exegeses@gmail.com>
2020-04-23 20:31:33 -0300

    array asociativo (ordenado por indice string)

--- inicial/clase4/arrays.php
+++ inicial/clase4/arrays.php
@@ -12,6 +12,7 @@
     print_r($italianos);
     echo '</pre>';
 
+    // asignación de índice numérico =>
     $italianos = [
                     8 => 'Ferrari', 'Fiat', 'Lamborghini',
                     15 => 'Bugatti', 'Maseratti', 'Alfa Romeo',
@@ -21,3 +22,36 @@
     echo '<pre>';
     print_r($italianos);
     echo '</pre>';
+
+
+    $celulares = [
+                    'j5',
+                    '11 pro',
+                    'e6',
+                    'Note7',
+                    'Mi A3',
+                    'Mate X',
+                    '1100'
+                ];
+
+    echo '<pre>';
+    print_r($celulares);
+    echo '</pre>';
+
+    // asignaciónde índice asociativo =>
+    $celulares = [
+                    'Samsung' => 'j5',
+                    'iPhone'=>'11 pro',
+                    'Motorola'=>'e6',
+                    'Redmi'=>'Note7',
+                    'Xiaomi'=>'Mi A3',
+                    'Huawei'=>'Mate X',
+                    'Nokia'=>'1100'
+                ];
+
+    echo '<pre>';
+    print_r($celulares);
+    echo '</pre>';
+
+
+    echo $celulares['Motorola'];
