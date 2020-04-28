9c905ecbf643881d890553ee4066b589596772c6
marcos pinardi <exegeses@gmail.com>
2020-04-28 19:52:40 -0300

    ejercicio listado groupon

---
+++ inicial/clase5/listadoGroupon.php
@@ -0,0 +1,5 @@
+<?php
+    $descripciones = ["Malla Apple Watch","Tour en bote", "Cedric, stand up show", "paseo en kayak", "cambio de aceite Midas", "Parking en el aeropuerto", "Spa con pileta", "Tobogan acuático"];
+    $precios = [9.99, 30, 29.75, 79, 22, 10.50, 21, 29.99];
+    $imagenes = ["apple-Watch", "bote", "cedric", "kayaks", "midas", "parking", "pileta", "tobogan"];
+?>
