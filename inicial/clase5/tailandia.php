63e0b262429424a2aa84e65303bcd969ea609059
marcos pinardi <exegeses@gmail.com>
2020-04-28 19:40:27 -0300

    locaciones de tailandia usando un bucle

--- inicial/clase5/tailandia.php
+++ inicial/clase5/tailandia.php
@@ -18,7 +18,40 @@
 					"tuk-tuk",
 					"wat-saket"
             ];
+	$n = 0;
+    $cantidad = count($locacion);
+?>
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Listado de locaciones</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <h1>Tailandia - locaciones</h1>
+        
+        <div class="row">
 
+<?php
+    while ( $n < $cantidad ) {
+?>
+            <div class="card col-3 p-1">
+                <img src="tailandia/<?= $locacion[$n] ?>.jpg" class="card-img-top">
+                <div class="card-body">
+                    <?= $locacion[$n]; ?>
+                </div>
+            </div>
+<?php
+        $n++;
+    }
 ?>
 
+        </div>
+        
+    </main>
+
+</body>
+</html>
 
