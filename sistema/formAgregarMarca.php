16ac09703d0bcadbb1af6ddc72198578ceaf6487
marcos pinardi <exegeses@gmail.com>
2020-05-07 21:42:28 -0300

    formulario de alta de un producto

--- sistema/formAgregarMarca.php
+++ sistema/formAgregarMarca.php
@@ -10,7 +10,7 @@
         <div class="alert alert-secondary p-4 col-8 mx-auto">
             <form action="agregarMarca.php" method="post">
                 Nombre de la marca: <br>
-                <input type="text" name="mkNombre" class="form-control">
+                <input type="text" name="mkNombre" class="form-control" required>
                 <br>
                 <button class="btn btn-dark mr-3 px-4">Agregar</button>
                 <a href="adminMarcas.php" class="btn btn-outline-secondary">
