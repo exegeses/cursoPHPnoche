550c5b4709cfa2303796b6f7266f1c61996a42bc
marcos pinardi <exegeses@gmail.com>
2020-05-07 21:25:44 -0300

    aplicacion para agregar una marca

--- sistema/funciones/marcas.php
+++ sistema/funciones/marcas.php
@@ -11,6 +11,17 @@
         return $resultado;
     }
 
+    function agregarMarca()
+    {
+        $mkNombre = $_POST['mkNombre'];
+        $link = conectar();
+        $sql = "INSERT INTO marcas
+                        ( mkNombre )
+                    VALUE
+                        ( '".$mkNombre."' )";
+        $resultado =  mysqli_query($link, $sql);
+        return $resultado;
+    }
     /*
      * listarMarcas()
      * verMarcaPorID()
