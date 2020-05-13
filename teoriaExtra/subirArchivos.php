5f4eff5bab730631cc36574d3b17288dd6ed3733
marcos pinardi <exegeses@gmail.com>
2020-05-12 21:04:53 -0300

    teoria para subir archivos

--- teoriaExtra/subirArchivos.php
+++ teoriaExtra/subirArchivos.php
@@ -6,9 +6,11 @@
     print_r($prdImagen);
     echo '</pre>';
 
+
+    /* rutina para subir archivo renombrado con timestamp */
     $dir = 'productos/';
     $temp = $_FILES['prdImagen']['tmp_name'];
-    $ext = pathinfo($_FILES['prdImagen']['name']);
+    $ext = pathinfo($_FILES['prdImagen']['name']);//info del archivo
     $prdImagen = time().'.'.$ext['extension']; //timestamp + extension
 
     move_uploaded_file($temp, $dir.$prdImagen);
