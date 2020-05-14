b4392c9704e332a3446d603933e46d82e63bcac1
marcos pinardi <exegeses@gmail.com>
2020-05-14 20:48:37 -0300

    listo formulario para modificar marca

--- sistema/formModificarMarca.php
+++ sistema/formModificarMarca.php
@@ -1,5 +1,8 @@
 <?php
     require 'config/config.php';
+    require 'funciones/conexion.php';
+    require 'funciones/marcas.php';
+    $marca = verMarcaPorID();
 	include 'includes/header.html';  
 	include 'includes/nav.php';  
 ?>
@@ -10,8 +13,9 @@
         <div class="alert alert-secondary p-4 col-8 mx-auto">
             <form action="modificarMarca.php" method="post">
                 Nombre de la marca: <br>
-                <input type="text" name="mkNombre" value="cañón" class="form-control" required>
+                <input type="text" name="mkNombre" value="<?= $marca['mkNombre'] ?>" class="form-control" required>
                 <br>
+                <input type="hidden" name="idMarca" value="<?= $marca['idMarca'] ?>">
                 <button class="btn btn-dark mr-3 px-4">Modificar</button>
                 <a href="adminMarcas.php" class="btn btn-outline-secondary">
                     Volver a panel
